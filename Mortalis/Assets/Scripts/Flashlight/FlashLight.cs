173854f34fec1281f3793485f3d428ce5bd546d5
Daegaz <dgzdaegaz0@gmail.com>
2025-07-15 17:36:38 -0500

    Puzzle,Bateria recargable

--- Mortalis/Assets/Scripts/Flashlight/FlashLight.cs
+++ Mortalis/Assets/Scripts/Flashlight/FlashLight.cs
@@ -2,11 +2,17 @@ using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+using UnityEngine.UI;
 public class FlashLight : MonoBehaviour
 {
     public GameObject lightObject;
     public AudioClip lightSound;
     private AudioSource audioSource;
+    public float EnergiaActual = 100;
+    public float EnergiaMaxima = 100;
+    public float consumeSpeed = 1;
+    public Image barraBateria;
+
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -20,10 +26,30 @@ public class FlashLight : MonoBehaviour
             LightManager();
 
         }
+
+        if (lightObject.activeSelf)
+        {
+            EnergiaActual -= Time.deltaTime * consumeSpeed;
+            if (EnergiaActual <= 0)
+            {
+                EnergiaActual = 0;
+                lightObject.SetActive(false);
+            }
+        }
+        else if (!lightObject.activeSelf)
+        {
+            EnergiaActual += Time.deltaTime * consumeSpeed;
+            if (EnergiaActual > EnergiaMaxima)
+            {
+                EnergiaActual = EnergiaMaxima;
+
+            }
+        }
+        barraBateria.fillAmount = EnergiaActual/EnergiaMaxima;
     }
     void LightManager()
     {
-        lightObject.SetActive(!lightObject.activeSelf);
+        lightObject.SetActive(!lightObject.activeSelf && EnergiaActual > 50);
         audioSource.PlayOneShot(lightSound);
     }
 }
